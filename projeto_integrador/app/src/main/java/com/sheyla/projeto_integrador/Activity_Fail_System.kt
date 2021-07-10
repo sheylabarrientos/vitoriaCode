5935bdefaca68c88cf30791b930795bfe08325a4
Sheyla Barrientos <sheylaaraujobarrientos@gmail.com>
2021-07-10 01:08:15 -0300

    delevopment_style

---
+++ projeto_integrador/app/src/main/java/com/sheyla/projeto_integrador/Activity_Fail_System.kt
@@ -0,0 +1,11 @@
+package com.sheyla.projeto_integrador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Fail_System : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fail_system)
+    }
+}
