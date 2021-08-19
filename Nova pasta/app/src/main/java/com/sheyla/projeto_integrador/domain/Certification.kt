b0d1da52a21e246f1bbb3d2d863d1af0f2249a9b
Sheyla Barrientos <sheylaaraujobarrientos@gmail.com>
2021-08-19 16:15:37 -0300

    refactor

--- Nova pasta/app/src/main/java/com/sheyla/projeto_integrador/domain/Certification.kt
+++
@@ -1,3 +0,0 @@
-package com.sheyla.projeto_integrador.domain
-
-class Certification(val certification: String, val type: Int)
--- Nova pasta/app/src/main/java/com/sheyla/projeto_integrador/presentation/MovieListener.kt
+++
@@ -1,9 +0,0 @@
-package com.sheyla.projeto_integrador.presentation
-
-import com.sheyla.projeto_integrador.domain.Movie
-
-interface MovieListener {
-    fun openMovieDetails(movieId: Int)
-    fun loadMoviesWithGenre(genreIds: List<Int>)
-    fun onFavoriteClickedListener(movie: Movie, isChecked: Boolean)
-}
